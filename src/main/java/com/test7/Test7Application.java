600bd349affc3c0da314c6a71773a89f109123e5
Rahul <53251733+Rahul-ix@users.noreply.github.com>
2025-03-14 19:24:31 +0530

    Update Test7Application.java

--- src/main/java/com/test7/Test7Application.java
+++ src/main/java/com/test7/Test7Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Test7Application {
 
+	int x=100;
 	public static void main(String[] args) {
 		SpringApplication.run(Test7Application.class, args);
 	}

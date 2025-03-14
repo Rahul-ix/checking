ffaa6065c82b6a0d2a86ae58fc878bae9aedb726
rahul <rahulkalal676@gmail.com>
2025-03-14 19:28:43 +0530

    second changes

--- src/main/java/com/test7/Test7Application.java
+++ src/main/java/com/test7/Test7Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test7Application {
 
 	int x=100;
+	boolean v=true;
 	public static void main(String[] args) {
 		SpringApplication.run(Test7Application.class, args);
 	}
